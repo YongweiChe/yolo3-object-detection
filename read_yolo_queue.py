4a7a1f241c16f506228ab3a61a4b1be22ff1995a
Chundao Che <cche@umich.edu>
2020-11-26 10:04:37 -0500

    add index.html

--- read_yolo_queue.py
+++ read_yolo_queue.py
@@ -2,8 +2,11 @@
 import pika
 import json
 import base64
+import glob
 
-image_location = "/var/www/html/image/"
+html_location = "/var/www/html/"
+image_folder = "image/"
+image_location = html_location + image_folder
 mq_web_key = 'webQ'
 mq_server_name = 'mq_server.yw.com'
 mq_server_port = '5672'
@@ -14,6 +17,25 @@ connection = pika.BlockingConnection(
              pika.ConnectionParameters(host=mq_server_name, credentials=credentials))
 channel = connection.channel()
 channel.queue_declare(queue=mq_web_key)
+
+#
+# writes html for links.html and sends it over to the web server
+#
+
+def update_past_images(html_location, image_location):
+        html_file = html_location + "links.html"
+        f = open(html_file, "w")
+        f.write('<html>')
+        counter = 0
+        for image in sorted(glob.glob(image_location + '*'), reverse = True):
+                #f.write('<a href = "' + image[9:] + '">Photo taken on ' + image[20:30] +' at ' + image[-9:-4] + '</a> <p> </p>')
+                prefix_len = len(html_location)
+                f.write('<a href = "' + image[prefix_len:] + '">' + image[prefix_len:] + '</a> <p> </p>')
+                counter += 1
+                if counter == 5:
+                        break
+        f.write('</html>')
+        f.close()
 
 def callback(ch, method, properties, body):
 	y = json.loads(body)
@@ -27,9 +49,13 @@ def callback(ch, method, properties, body):
 
 	print("Received file from:  %r" % method.routing_key)
 
+	update_past_images(html_location, image_location)
+
+# main
 channel.basic_consume(
         queue=mq_web_key, on_message_callback=callback, auto_ack=True)
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 
 channel.start_consuming()
+
