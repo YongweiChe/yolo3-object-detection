3cf4db260ed501517d5756f5b45f84d8a3d8ebd4
Chundao Che <cche@umich.edu>
2020-11-28 09:33:22 -0500

    Fix tabs

--- read_yolo_queue.py
+++ read_yolo_queue.py
@@ -15,7 +15,7 @@ default_username = 'test'   # CHANGE me or use key
 default_password = 'test'   # CHANGE me or use key
 credentials = pika.PlainCredentials(username=default_username, password=default_password)
 connection = pika.BlockingConnection(
-             pika.ConnectionParameters(host=mq_server_name, credentials=credentials))
+         pika.ConnectionParameters(host=mq_server_name, credentials=credentials))
 channel = connection.channel()
 channel.queue_declare(queue=mq_web_key)
 
@@ -24,7 +24,6 @@ channel.queue_declare(queue=mq_web_key)
 #
 def update_image_html(image_name):
     html_code = '<html> <p style = "font-size: 100%">Most recent image: ' + image_name + '</p> <img src = ' + image_folder + image_name + ' alt = "image" style = "width:500px"></html>'
-    
     f = open(html_location + "image.html", "w")
     f.write(html_code)
     f.close()
@@ -33,19 +32,18 @@ def update_image_html(image_name):
 # writes html for links.html and sends it over to the web server
 #
 def update_past_images(html_location):
-        html_file = html_location + "links.html"
-        f = open(html_file, "w")
-        f.write('<html>')
-        counter = 0
-        for image in sorted(glob.glob(image_location + '*'), reverse = True):
-                prefix_len = len(html_location)
-                f.write('<a href = "' + image[prefix_len:] + '">' + image[prefix_len:] + '</a> <p> </p>')
-                counter += 1
-                if counter >= image_retention_max:
-                        break
-        f.write('</html>')
-        f.close()
-
+    html_file = html_location + "links.html"
+    f = open(html_file, "w")
+    f.write('<html>')
+    counter = 0
+    for image in sorted(glob.glob(image_location + '*'), reverse = True):
+        prefix_len = len(html_location)
+        f.write('<a href = "' + image[prefix_len:] + '">' + image[prefix_len:] + '</a> <p> </p>')
+        counter += 1
+        if counter >= image_retention_max:
+            break
+    f.write('</html>')
+    f.close()
 
 def callback(ch, method, properties, body):
     y = json.loads(body)
@@ -53,19 +51,18 @@ def callback(ch, method, properties, body):
     content = y["content"]
     if(y["name"].find('.jpg') != -1):
         content = base64.b64decode(content)
-    f = open(image_location + y["name"], "w")
-    f.write(content)
-    f.close()
+        f = open(image_location + y["name"], "w")
+        f.write(content)
+        f.close()
 
     print("Received file from:  %r" % method.routing_key)
 
     update_past_images(html_location)
-    print("image name = %r" % y["name"])
     update_image_html(y["name"])
 
 # main
 channel.basic_consume(
-        queue=mq_web_key, on_message_callback=callback, auto_ack=True)
+    queue=mq_web_key, on_message_callback=callback, auto_ack=True)
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 
